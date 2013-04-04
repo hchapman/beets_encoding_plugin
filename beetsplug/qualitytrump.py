5dcc237bf41fed03fb4bf6fdbf350b5eca6a7c8e
Harrison Chapman <hchapman@bowdoin.edu>
2013-04-04 15:38:24 -0400

    Added a rudimentary  template field which returns either the format of the track or the mp3 encoding preset, if available

--- beetsplug/qualitytrump.py
+++ beetsplug/qualitytrump.py
@@ -22,6 +22,32 @@ def f():
         item = yield
         print item
 
+def get_item_quality(item):
+    preset = None
+    if item.format == u'MP3':
+        audio_file = core.load(item.path)
+        lt = audio_file.info.lame_tag
+        if not audio_file or not audio_file.info.lame_tag:
+            pass
+        elif lt.has_key('preset'):
+            preset = lt['preset']
+    return {"preset": preset,
+            "bitrate": item.bitrate,
+            "format": item.format}
+
+def get_album_quality(album):
+    formats = []
+    bitrates = []
+    presets = []
+    for track in album.items():
+        q = get_item_quality(track)
+        presets.append(q["preset"])
+        formats.append(q["format"])
+        bitrates.append(q["bitrate"])
+    return {"bitrate": sum(bitrates)/len(bitrates),
+            "format": formats[0] if formats.count(formats[0]) == len(formats) else "Mutt",
+            "preset": presets[0] if presets.count(presets[0]) == len(presets) else "Mutt"}
+
 fmt_cmd = ui.Subcommand('fmt', help="do some format nonsense")
 def fmt(lib, opts, args):
     threads = 3
@@ -29,49 +55,20 @@ def fmt(lib, opts, args):
     if opts.album:
         albums = lib.albums(ui.decargs(args))
     else:
+        albums = []
         items = lib.items(ui.decargs(args))
     
     # Try to determine the quality of the album
     if albums:
         for album in albums:
-            formats = []
-            bitrates = []
-            presets = []
-            for track in album.items():
-                if track.format == u'MP3':
-                    audio_file = core.load(track.path)
-                    lt = audio_file.info.lame_tag
-                    if not audio_file or not audio_file.info.lame_tag:
-                        presets.append(None)
-                    elif lt.has_key('preset'):
-                        preset = lt['preset']
-                        presets.append(preset)
-                formats.append(track.format)
-                bitrates.append(track.bitrate)
-            print
             print album.album
-            print formats
-            print bitrates
-            print "======="
-            album_is_mp3 = False
-            if formats.count(formats[0]) == len(formats):
-                print "The album is %s encoded" %formats[0]
-                album_is_mp3 = formats[0] == "MP3"
-            else:
-                print "This is a mutt: format mismatch"
-                print formats
-            if album_is_mp3:
-                abr = sum(bitrates)/len(bitrates)
-                print "The average bitrate of the mp3s is %s"%abr
-                print "It was probably encoded as %s"%guess_bitrate(abr/1000)
-                if presets.count(presets[0]) == len(presets):
-                    if presets[0]:
-                        print "LAME header says it was encoded with preset %s"%presets[0]
-                    else:
-                        print "No LAME header or not encoded with a preset"
-                else:
-                    print "This is a mutt: preset mismatch"
-                    print presets
+            print get_album_quality(album)
+            print _tmpl_quality(album.items().next())
+    elif items:
+        for item in items:
+            print item.title
+            print get_item_quality(item)
+            print _tmpl_quality(item)
 
 fmt_cmd.func = fmt
 fmt_cmd.parser.add_option("-a", "--album", action="store_true", help="poop")
@@ -79,3 +76,22 @@ fmt_cmd.parser.add_option("-a", "--album", action="store_true", help="poop")
 class QualityTrumper(BeetsPlugin):
     def commands(self):
         return [fmt_cmd]
+
+@QualityTrumper.template_field('quality')
+def _tmpl_quality(item):
+    """Expand to the encode quality of the track. Anything but MP3s
+    will expand to their media type (e.g. FLAC, AAC), or Mutt if they
+    are a combination of multiple formats. MP3s will expand to 'MP3
+    $preset', where $preset is the LAME preset, Mutt if the album
+    consists of multiple presets, or None if the album has no preset
+    data.
+    """
+    if item.album:
+        q = get_item_quality(item)
+    else:
+        q = get_item_quality(item)
+
+    if q["format"] == u'MP3':
+        return u"MP3 %s"%("None" if not q["preset"] else q["preset"])
+    else:
+        return u"None" if not q["format"] else q["format"]
